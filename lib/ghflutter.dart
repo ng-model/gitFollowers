724eb7a144df1dcf0d4aaa8a01ad107aea36b6aa
manoj <manoj.maduri1@gmail.com>
2019-03-04 22:23:11 -0500

    Update ghflutter.dart

--- lib/ghflutter.dart
+++ lib/ghflutter.dart
@@ -33,7 +33,21 @@ class GHFlutterState extends State<GHFlutter> {
             final index = position ~/ 2;
 
             return _buildRow(index);
+             
           }),
+         floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+          context,
+          new MaterialPageRoute(
+            builder: (context) => new FollowerDetailsPage(
+                ),
+          )
+          );
+        },
+        child: new Icon(Icons.add),
+         ),
+          
     );
   }
 
@@ -45,7 +59,8 @@ class GHFlutterState extends State<GHFlutter> {
           leading: new CircleAvatar(
               backgroundColor: Colors.green,
               backgroundImage: new NetworkImage(_members[i].avatarUrl)),
-        ));
+        ),
+        );
   }
 
   _loadData() async {
@@ -58,24 +73,22 @@ class GHFlutterState extends State<GHFlutter> {
         final member =
             new Member(memberJSON["login"], memberJSON["avatar_url"]);
         _members.add(member);
-        onTap:
-        () {
-          // Navigator.push(
-          // context,
-          new MaterialPageRoute(
-            builder: (context) => new FollowerDetailsPage(
-                // title: 'Follower Details',
-                // counter: widget.counter,
-                ),
-          );
-          // );
-        };
+        
       }
     });
   }
 }
 
 class GHFlutter extends StatefulWidget {
+  GHFlutter({
+    Key key,
+    this.title,
+    this.detail
+  }) :super(key: key);
+  final String title;
+  final String detail;
   @override
   createState() => new GHFlutterState();
 }
+
+
